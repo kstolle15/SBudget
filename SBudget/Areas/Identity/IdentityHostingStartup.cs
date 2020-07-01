c0d96ee7e160aa3d42a3a728cb646c8572ccbd53
kstolle15 <stollekyle@gmail.com>
2020-07-01 13:18:17 -0500

    Added initial identity scaffold

---
+++ SBudget/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SBudget.Areas.Identity.Data;
+using SBudget.Data;
+
+[assembly: HostingStartup(typeof(SBudget.Areas.Identity.IdentityHostingStartup))]
+namespace SBudget.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SBudgetContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SBudgetContextConnection")));
+
+                services.AddDefaultIdentity<SBudgetUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<SBudgetContext>();
+            });
+        }
+    }
+}
