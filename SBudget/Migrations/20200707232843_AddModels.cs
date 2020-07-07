40187cd35f945ec65e8c0223723f7deb417fc868
kstolle15 <stollekyle@gmail.com>
2020-07-07 18:31:07 -0500

    Fixed migrations and Finished Initial backend setup

---
+++ SBudget/Migrations/20200707232843_AddModels.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SBudget.Migrations
+{
+    public partial class AddModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    userId = table.Column<string>(nullable: true),
+                    type = table.Column<int>(nullable: false),
+                    balance = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Accounts_AspNetUsers_userId",
+                        column: x => x.userId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Budgets",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Budgets", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Budgets_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Expenses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AccountID = table.Column<int>(nullable: true),
+                    Amount = table.Column<double>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    BudgetID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expenses", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Expenses_Accounts_AccountID",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Expenses_Budgets_BudgetID",
+                        column: x => x.BudgetID,
+                        principalTable: "Budgets",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Incomes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AccountID = table.Column<int>(nullable: true),
+                    Amount = table.Column<double>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Type = table.Column<int>(nullable: false),
+                    BudgetID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incomes", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Incomes_Accounts_AccountID",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Incomes_Budgets_BudgetID",
+                        column: x => x.BudgetID,
+                        principalTable: "Budgets",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transfers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    OldAccountID = table.Column<int>(nullable: true),
+                    NewAccountID = table.Column<int>(nullable: true),
+                    Amount = table.Column<double>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    BudgetID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transfers", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Transfers_Budgets_BudgetID",
+                        column: x => x.BudgetID,
+                        principalTable: "Budgets",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Transfers_Accounts_NewAccountID",
+                        column: x => x.NewAccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Transfers_Accounts_OldAccountID",
+                        column: x => x.OldAccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_userId",
+                table: "Accounts",
+                column: "userId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Budgets_UserId",
+                table: "Budgets",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_AccountID",
+                table: "Expenses",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_BudgetID",
+                table: "Expenses",
+                column: "BudgetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incomes_AccountID",
+                table: "Incomes",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incomes_BudgetID",
+                table: "Incomes",
+                column: "BudgetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transfers_BudgetID",
+                table: "Transfers",
+                column: "BudgetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transfers_NewAccountID",
+                table: "Transfers",
+                column: "NewAccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transfers_OldAccountID",
+                table: "Transfers",
+                column: "OldAccountID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Expenses");
+
+            migrationBuilder.DropTable(
+                name: "Incomes");
+
+            migrationBuilder.DropTable(
+                name: "Transfers");
+
+            migrationBuilder.DropTable(
+                name: "Budgets");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
